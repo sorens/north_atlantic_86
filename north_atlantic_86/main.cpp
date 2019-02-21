73a2755cb03604c592340407366660e3ba71d6de
Steve Orens <steve@orens.com>
2019-02-20 20:13:25 -0800

    Integrate argh command line processor

--- north_atlantic_86/main.cpp
+++ north_atlantic_86/main.cpp
@@ -19,6 +19,7 @@
 #include "ship_data.hpp"
 #include "weapon_data.hpp"
 
+#include "argh.h"
 #include "tests.hpp"
 
 #include <fstream>
@@ -27,46 +28,85 @@
 
 #include "task_force.hpp"
 
-int main(int argc, const char * argv[]) {
-    
+void play_game()
+{
     try
     {
-        auto tests = Tests::Make();
-        tests->task_force_unit_test();
+        loginfo("********* GAME STARTING *********");
         
-//        loginfo("********* GAME STARTING *********");
-//        
-//        // read in map data
-//        std::string map_data = MapSetup::Import_Data("map_data.json");
-//        
-//        // read in ship_data.json
-//        std::string ship_data = ShipData::Import_Data("ship_data.json");
-//       
-//        // read in weapon_data.json
-//        std::string weapon_data = WeaponData::Import_Data("weapon_data.json");
-//        
-//        auto setup_data = MapSetup::factory(map_data);
-//        auto game = Game::Make(setup_data, ship_data, weapon_data);
-//        game->add_nato_player("Sally");
-//        game->add_soviet_player("Yuri");
-//        auto display = MapDisplayAscii::Generate(game);
-//        std::cout << display.str();
-//
-//        // take a turn
-//        game->next_turn();
+        // read in map data
+        std::string map_data = MapSetup::Import_Data("map_data.json");
+        
+        // read in ship_data.json
+        std::string ship_data = ShipData::Import_Data("ship_data.json");
+       
+        // read in weapon_data.json
+        std::string weapon_data = WeaponData::Import_Data("weapon_data.json");
+        
+        auto setup_data = MapSetup::factory(map_data);
+        auto game = Game::Make(setup_data, ship_data, weapon_data);
+        game->add_nato_player("Sally");
+        game->add_soviet_player("Yuri");
+        auto display = MapDisplayAscii::Generate(game);
+        std::cout << display.str();
+
+        // take a turn
+        game->next_turn();
     }
     catch(ship_data_exception &e)
     {
-        std::cout << "ship_data_exception => '" << e.what() << "'" << std::endl;
+        logerror("ship_data_exception => '" << e.what() << "'");
     }
     catch(std::exception &e)
     {
-        std::cout << "std::exception => '" << e.what() << "'" << std::endl;
+        logerror("std::exception => '" << e.what() << "'");
     }
     catch(...)
     {
-        std::cout << "unhandled exception" << std::endl;
+        logerror("unhandled exception");
+    }
+}
+
+void print_help()
+{
+    std::cout << "North Atlantic '86 (© Stephen Orens, 2019)" << std::endl;
+    std::cout << "A recreation of the famous Gary Grigsby game!" << std::endl;
+    std::cout << "usage: north_atlantic_86 -u|-p" << std::endl;
+    std::cout << "        -u, --unit_tests        run the unit tests" << std::endl;
+    std::cout << "        -p, --play_game         play the game" << std::endl;
+}
+
+void run_unit_tests()
+{
+    try
+    {
+       Tests::Make()->test_all();
+    }
+    catch(...)
+    {
+        logerror("unhandled exception");
+    }
+}
+
+int main(int argc, const char * argv[]) {
+
+    argh::parser cmdl(argc, argv);
+    
+    if (cmdl[{"-u", "--unit-tests"}]) {
+        run_unit_tests();
+        return 0;
     }
     
+    if (cmdl[{"-p", "--play"}]) {
+        play_game();
+        return 0;
+    }
+    
+    if (cmdl[{"-h", "-?", "--help"}]) {
+        print_help();
+        return 0;
+    }
+    
+    print_help();
     return 0;
 }
